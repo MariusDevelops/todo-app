69ab227baef52dfbdaa903017fedd88d4a754201
MariusDevelops <brazasmarius@gmail.com>
2021-10-05 22:54:44 +0200

    Theme switcher

---
+++ main.js
@@ -0,0 +1,7 @@
+
+// THEME SWITCHER
+const theme = document.getElementById('theme');
+
+theme.addEventListener('click', () => {
+    document.querySelector('body').classList = [theme.checked ? 'theme-light' : 'theme-dark'];
+});
